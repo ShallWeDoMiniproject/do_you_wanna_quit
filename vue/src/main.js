52bbc149e8f0f69090a783bfc39ae53590220ded
hyuk.kim <hyuk.kim@konantech.com>
2022-03-14 14:17:25 +0900

    Vuetify 셋팅 완료

--- vue/src/main.js
+++ vue/src/main.js
@@ -1,10 +1,15 @@
 import Vue from 'vue';
 import App from './App.vue';
 import store from './store';
+import Vuex from 'vuex';
+import vuetify from './plugins/vuetify';
+
+Vue.use(Vuex);
 
 Vue.config.productionTip = false;
 
 new Vue({
     store,
+    vuetify,
     render: (h) => h(App),
 }).$mount('#app');
